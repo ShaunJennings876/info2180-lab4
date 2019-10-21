41fcef2595b946cc543e4c351feced8a2281df25
Shaun Jennings <shaun.sj97@gmail.com>
2019-10-21 15:27:11 -0500

    Update

--- info2180-lab4-tic-tac-toe-master/tic-tac-toe.js
+++ info2180-lab4-tic-tac-toe-master/tic-tac-toe.js
@@ -3,10 +3,26 @@
 function main (){
   window.onload = function () {
     makeSquares();
-    /*let squares = document.getElementsByClassName("squares");
+    clearSquares();
+
+    const arr = ["","","","","","","","",""];
+    let state = "";
+
+    trackGame(arr,state);
+    /*squares = document.getElementsByClassName('square')
     for(let i = 0; i < squares.length; i++){
-      squares[i].innerHTML = "";
+      squares[i].onclick = function(){
+        state = updateSquares(arr,this,state,i);
+      }
     }*/
+
+  }
+}
+
+function clearSquares(){
+  squares = document.getElementsByClassName("squares");
+  for(let i = 0; i < squares.length; i++){
+    squares[i].innerHTML = "";
   }
 }
 
@@ -15,6 +31,48 @@ function makeSquares(){
   for(let i = 0; i < squares.length; i++){
     squares[i].classList.add("square");
   }
+}
+
+function drawX(sqr){
+  sqr.className = "square X";
+  sqr.innerHTML = "X";
+}
+
+function drawO(sqr){
+  sqr.className = "square O";
+  sqr.innerHTML = "O";
+}
+
+function updateSquares(arr,sqr,state,pos){
+
+  if(state === ""){
+    drawX(sqr);
+    arr[pos] = "X";
+    return "X";
+  }
+  else if(state === "X" && arr[pos] === ""){
+    drawO(sqr);
+    arr[pos] = "O";
+    return "O";
+  }
+  else if(state === "O" && arr[pos] === ""){
+    drawX(sqr);
+    arr[pos] = "X";
+    return "X"
+  }
+  return state;
+
+}
+
+function trackGame(arr,state){
+
+  squares = document.getElementsByClassName('square')
+  for(let i = 0; i < squares.length; i++){
+    squares[i].onclick = function(){
+      state = updateSquares(arr,this,state,i);
+    }
+  }
+
 }
 
 main ();
